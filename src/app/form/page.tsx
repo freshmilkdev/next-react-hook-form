567a599b267a1dc972bab63c83914ebf3fb6e4e8
max <max.ruchkin@gmail.com>
2023-07-17 14:47:26 +0300

    home page navigation example

---
+++ src/app/form/page.tsx
@@ -0,0 +1,9 @@
+import Form from "@/components/Form";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-50">
+      <Form/>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Form from "@/components/Form";
+import Link from 'next/link';
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-50">
-      <Form/>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen items-center justify-center bg-slate-50">
+           <div className='flex flex-col text-lg'>
+               <h1 className='text-2xl'>Register Forms</h1>
+               <Link href='form' className='my-4 text-blue-400'>Custom validation</Link>
+               <Link href='form-yup' className='text-blue-400'>Yup validation</Link>
+           </div>
+        </main>
+    )
 }
